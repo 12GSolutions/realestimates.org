7b9a2ceca1eb2b50b2b719d47b51693b9011b898
Maxime Rousseaux-Bridle <max@12gsolutions.com>
2013-06-25 00:57:18 -0400

    Testing a scroll to method

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,11 @@
+
 function RealEstimate($scope) {
   $scope.estimate = 0;
   $scope.risk = 1;
   $scope.buffer = 1.25;
+  $scope.estimateChanged = function() {
+    location.href="#";location.href="#realEstimate";
+  }
 }
 
 angular.element(document).ready(function () {
